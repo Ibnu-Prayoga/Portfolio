7944d43a1cec154424c27d7faff5f05eff82222a
Ibnu-Prayoga <doubleuu01@gmail.com>
2024-10-24 07:12:22 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,3 +20,24 @@ links.forEach(link => {
     navbar.classList.remove('active');
   });
 });
+
+menuToggle.addEventListener('click', function (e) {
+  e.preventDefault(); // Mencegah default action (mencegah link kembali ke home)
+  menu.classList.toggle('open');
+  menuToggle.classList.toggle('active');
+
+  // Ubah ikon menjadi X ketika menu dibuka dan kembali ke hamburger saat ditutup
+  if (menuToggle.classList.contains('active')) {
+    menuIcon.innerHTML = `
+      <path d="M3 3l18 18M3 21l18-18"/>
+    `;
+  } else {
+    menuIcon.innerHTML = `
+      <path d="M3 6h18v2H3V6zm0 5h18v2H3v-2zm0 5h18v2H3v-2z"/>
+    `;
+  }
+});
+
+
+
+
